1a78eaff0bdfb01539309950f4009b5ea371533a
zhulin <2897669115@qq.com>
2022-09-10 15:45:39 +0800

    添加百度统计

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -13,15 +13,13 @@ module.exports = {
         ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no'}],
         ['meta', {name: 'keywords', content: 'Sunspot,博客,zhulinz.top,zhulin'}],
         ["script", {src: "https://kit.fontawesome.com/5ec517f380.js", crossorigin: "anonymous"}],
-        ['script', {}, `
-        var _hmt = _hmt || [];
-        (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?1fa46dc84d9c2e74f953957f03bded00";
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s);
-        })();
-        })();`]
+        ['script', {},'var _hmt = _hmt || [];\n' +
+        '            (function() {\n' +
+        '                var hm = document.createElement("script");\n' +
+        '                hm.src = "https://hm.baidu.com/hm.js?1fa46dc84d9c2e74f953957f03bded00";\n' +
+        '                var s = document.getElementsByTagName("script")[0]; \n' +
+        '                s.parentNode.insertBefore(hm, s);\n' +
+        '            })();'],
     ],
     markdown: {
         lineNumbers: true,
