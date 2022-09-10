340cf275cb241bfe8d8e4155275df0c4589ec713
zhulin <2897669115@qq.com>
2022-09-10 16:04:32 +0800

    添加谷歌统计

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -13,7 +13,7 @@ module.exports = {
         ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no'}],
         ['meta', {name: 'keywords', content: 'Sunspot,博客,zhulinz.top,zhulin'}],
         ["script", {src: "https://kit.fontawesome.com/5ec517f380.js", crossorigin: "anonymous"}],
-        ['script', {},'var _hmt = _hmt || [];\n' +
+        ['script', {}, 'var _hmt = _hmt || [];\n' +
         '            (function() {\n' +
         '                var hm = document.createElement("script");\n' +
         '                hm.src = "https://hm.baidu.com/hm.js?1fa46dc84d9c2e74f953957f03bded00";\n' +
@@ -154,6 +154,11 @@ module.exports = {
             lowercase: true, // Converted into lowercase, default: true
             separator: '-' // Separator of the slug, default: '-'
         }],
+        //谷歌统计
+        ['@vuepress/google-analytics', {
+            'ga': 'UA-233962372-2' // UA-00000000-0
+        }
+        ],
         // 音乐插件
         ['meting', {
             //metingApi: "https://meting.sigure.xyz/api/music",
