c3b9035ec164419aaa0853e603dab83893564e91
zhulin <2897669115@qq.com>
2022-09-18 22:02:30 +0800

    接入微信分享功能

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -18,13 +18,13 @@ module.exports = {
             lang: 'zh-CN'
         }
     },
-    title: 'Sunspot\'s Blog',
-    description: 'Follow your heart，see night！',
+    title: '屋后一片•小竹林',
+    description: '随心所往，看见未来。',
     dest: 'public',
     head: [
         ['link', {rel: 'icon', href: 'https://oss.zhulinz.top/newImage/202209082357267.ico'}],
         ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no'}],
-        ['meta', {name: 'keywords', content: 'Sunspot,博客,zhulinz.top,zhulin'}],
+        ['meta', {name: 'keywords', content: 'Sunspot,博客,zhulinz.top,zhulin,祝林,竹林,Java'}],
         ['meta', {name: 'baidu-site-verification', content: 'code-rGCUXPJ1FA'}],
         ["script", {src: "https://kit.fontawesome.com/5ec517f380.js", crossorigin: "anonymous"}],
         ['script', {}, 'var _hmt = _hmt || [];\n' +
