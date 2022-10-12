38e61269806c7caca0c15001b4d8d433904b9cc1
zhulin <2897669115@qq.com>
2022-10-12 09:58:39 +0800

    添加书单指南

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,16 +1,3 @@
-const extendsNetworks = {
-    pinterest: {
-        sharer: `https://pinterest.com/pin/create/button/?url=@url&media=@media&description=@title`,
-        type: `popup`,
-        icon: `/pinterest.png`,
-    },
-    linkedin: {
-        sharer: `https://www.linkedin.com/shareArticle?mini=true&url=@url&title=@title&summary=@description`,
-        type: `popup`,
-        color: `#1786b1`,
-        icon: `<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path d="M910.336 0H113.664A114.005333 114.005333 0 0 0 0 113.664v796.672A114.005333 114.005333 0 0 0 113.664 1024h796.672A114.005333 114.005333 0 0 0 1024 910.336V113.664A114.005333 114.005333 0 0 0 910.336 0zM352.256 796.330667H207.189333V375.466667h145.066667z m-72.021333-477.866667a77.824 77.824 0 0 1-81.237334-74.069333A77.824 77.824 0 0 1 280.234667 170.666667a77.824 77.824 0 0 1 81.237333 73.728 77.824 77.824 0 0 1-81.237333 73.386666z m582.314666 477.866667H716.8v-227.669334c0-46.762667-18.432-93.525333-73.045333-93.525333a84.992 84.992 0 0 0-81.237334 94.549333v226.304h-140.629333V375.466667h141.653333v60.757333a155.989333 155.989333 0 0 1 136.533334-71.338667c60.416 0 163.498667 30.378667 163.498666 194.901334z" /></svg>`,
-    },
-}
 const timeUpdate = new Date().getTime();
 module.exports = {
     // base: '/blog/',
@@ -52,8 +39,9 @@ module.exports = {
     themeConfig: {
         nav: [
             {text: '主页', link: '/', icon: 'reco-home'},
-            {text: "文档", link: "/docs/", icon: "reco-message",},
+            {text: "文档", link: "/docs/", icon: 'reco-document',},
             {text: '归档', link: '/archives.html', icon: 'reco-date'},
+            {text: '书单指南', link: '/booksGuide.html', icon: 'reco-suggestion'},
             {text: '关于', link: '/about.html', icon: 'reco-account'},
         ],
         //自定义侧边栏-左
